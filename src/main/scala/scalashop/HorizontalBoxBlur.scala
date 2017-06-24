1ca8c254489a012a663645ee5c2b3c1b99741d38
nabih <en_nebbache@esi.dz>
2017-06-25 00:01:13 +0100

    Initial commit

---
+++ src/main/scala/scalashop/HorizontalBoxBlur.scala
@@ -0,0 +1,71 @@
+package scalashop
+
+import org.scalameter._
+import common._
+
+object HorizontalBoxBlurRunner {
+
+  val standardConfig = config(
+    Key.exec.minWarmupRuns -> 5,
+    Key.exec.maxWarmupRuns -> 10,
+    Key.exec.benchRuns -> 10,
+    Key.verbose -> true
+  ) withWarmer(new Warmer.Default)
+
+  def main(args: Array[String]): Unit = {
+    val radius = 3
+    val width = 1920
+    val height = 1080
+    val src = new Img(width, height)
+    val dst = new Img(width, height)
+    val seqtime = standardConfig measure {
+      HorizontalBoxBlur.blur(src, dst, 0, height, radius)
+    }
+    println(s"sequential blur time: $seqtime ms")
+
+    val numTasks = 32
+    val partime = standardConfig measure {
+      HorizontalBoxBlur.parBlur(src, dst, numTasks, radius)
+    }
+    println(s"fork/join blur time: $partime ms")
+    println(s"speedup: ${seqtime / partime}")
+  }
+}
+
+
+/** A simple, trivially parallelizable computation. */
+object HorizontalBoxBlur {
+
+  /** Blurs the rows of the source image `src` into the destination image `dst`,
+   *  starting with `from` and ending with `end` (non-inclusive).
+   *
+   *  Within each row, `blur` traverses the pixels by going from left to right.
+   */
+  def blur(src: Img, dst: Img, from: Int, end: Int, radius: Int): Unit = {
+    for {
+      i <- from until end
+      j <- 0 until src.width
+    }{
+      dst(j, i) = boxBlurKernel(src , j , i , radius)}
+  }
+
+  /** Blurs the rows of the source image in parallel using `numTasks` tasks.
+   *
+   *  Parallelization is done by stripping the source image `src` into
+   *  `numTasks` separate strips, where each strip is composed of some number of
+   *  rows.
+   */
+  def parBlur(src: Img, dst: Img, numTasks: Int, radius: Int): Unit = {
+    val lineNumber = src.height
+    val stripLength = if(lineNumber % 2 == 0) lineNumber / numTasks else (lineNumber +1) /numTasks
+    val range  = if(src.height > numTasks)0 until lineNumber grouped(stripLength)
+    else 0 until lineNumber grouped(1)
+
+    val tasks = for {
+      i <- range
+    }yield task(blur( src , dst , i.head , i.last+1 , radius))
+
+    tasks.foreach(task => task.join())
+  }
+
+}
